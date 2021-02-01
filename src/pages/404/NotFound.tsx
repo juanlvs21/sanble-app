b8f2430ca8511ad951814cb0f4c01b4d137d1174
Juan Villarroel <juanlvs97@gmail.com>
2021-02-01 12:35:54 -0400

    404 page

---
+++ src/pages/404/NotFound.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { IonButton } from "@ionic/react";
+
+// Styles
+import styles from "./NotFound.module.css";
+
+// Images
+import error from "../../assets/images/error.png";
+
+const NotFound: React.FC = () => {
+  return (
+    <div className={styles.not_found}>
+      <img src={error} alt="" />
+      <h1>Página no encontrada</h1>
+
+      <IonButton color="light" routerLink="/">
+        Ir al inicio
+      </IonButton>
+    </div>
+  );
+};
+
+export default NotFound;
