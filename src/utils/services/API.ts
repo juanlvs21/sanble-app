21790e907a45f0115c6842f6a3c01a733ccf1d49
Juan Villarroel <juanlvs97@gmail.com>
2021-02-08 14:19:26 -0400

    Login with firebase | Get current session

--- src/utils/services/API.ts
+++ src/utils/services/API.ts
@@ -4,24 +4,6 @@ import formatMessageToasts from "../formatMessageToasts";
 const API_URL = process.env.REACT_APP_URL_API;
 
 // Auth
-export const login: any = (body: any): Promise<Response> => {
-  return new Promise<any>(async (resolve, reject) => {
-    try {
-      const response = await fetch(`${API_URL}/auth/login`, {
-        method: "POST",
-        body: JSON.stringify(body),
-      });
-      const { data } = await response.json();
-
-      if (!response.ok) {
-        reject(formatMessageToasts(data));
-      } else resolve(data);
-    } catch (error) {
-      console.log(error);
-      reject("Error desconocido");
-    }
-  });
-};
 export const register: any = (body: any): Promise<Response> => {
   return new Promise<any>(async (resolve, reject) => {
     try {
@@ -31,26 +13,6 @@ export const register: any = (body: any): Promise<Response> => {
       });
       const { data } = await response.json();
 
-      if (!response.ok) {
-        reject(formatMessageToasts(data));
-      } else resolve(data);
-    } catch (error) {
-      console.log(error);
-      reject("Error desconocido");
-    }
-  });
-};
-export const checkSession: any = (token: string): Promise<Response> => {
-  return new Promise<any>(async (resolve, reject) => {
-    try {
-      const response = await fetch(`${API_URL}/auth/check`, {
-        method: "POST",
-        headers: new Headers({
-          Authorization: token,
-        }),
-      });
-
-      const { data } = await response.json();
       if (!response.ok) {
         reject(formatMessageToasts(data));
       } else resolve(data);
