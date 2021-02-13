1488e0ca83c0765692c08874bcb36a2e1b85d70d
Juan Villarroel <juanlvs97@gmail.com>
2021-02-12 23:26:39 -0400

    Changes in Register, Slides and sanble.apk

--- src/components/slides/Slide.tsx
+++ src/components/slides/Slide.tsx
@@ -34,38 +34,42 @@ const Slide: React.FC<ContainerProps> = ({
   return (
     <IonSlide className={styles.slide}>
       <div className={styles.container} style={{ backgroundColor }}>
-        <img className={styles.image} src={image} alt={title} />
+        <div className={styles.containerContent}>
+          <img className={styles.image} src={image} alt={title} />
 
-        {end ? (
-          <>
-            <p className={styles.description}>{title}</p>
-            <h1 className={styles.title}>{description}</h1>
-            <IonButton
-              expand="block"
-              color="dark"
-              className={styles.btn}
-              onClick={goLogin}
-            >
-              Comenzar
-            </IonButton>
-          </>
-        ) : (
-          <>
-            <h1 className={styles.title}>{title}</h1>
-            <p className={styles.description}>{description}</p>
+          {end ? (
+            <>
+              <p className={styles.description}>{title}</p>
+              <h1 className={styles.title}>{description}</h1>
+              <IonButton
+                expand="block"
+                color="dark"
+                className={styles.btn}
+                onClick={goLogin}
+              >
+                Comenzar
+              </IonButton>
+            </>
+          ) : (
+            <>
+              <h1 className={styles.title}>{title}</h1>
+              <p className={styles.description}>{description}</p>
+            </>
+          )}
+        </div>
 
-            <div className={styles.arrowsContainer}>
-              {i > 0 && (
-                <IonIcon icon={arrowBackOutline} className={styles.arrowLeft} />
-              )}
-              {i < length && (
-                <IonIcon
-                  icon={arrowForwardOutline}
-                  className={styles.arrowRight}
-                />
-              )}
-            </div>
-          </>
+        {!end && (
+          <div className={styles.arrowsContainer}>
+            {i > 0 && (
+              <IonIcon icon={arrowBackOutline} className={styles.arrowLeft} />
+            )}
+            {i < length && (
+              <IonIcon
+                icon={arrowForwardOutline}
+                className={styles.arrowRight}
+              />
+            )}
+          </div>
         )}
       </div>
     </IonSlide>
