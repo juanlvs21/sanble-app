55a19ef7a9048d6970ed8211ef2df2d392e7d01a
Juan Villarroel <juanlvs97@gmail.com>
2021-02-28 14:55:31 -0400

    Stand details: clean data and optimization

--- src/components/skeleton/stands/Details.tsx
+++ src/components/skeleton/stands/Details.tsx
@@ -1,9 +1,36 @@
 import React from "react";
+import { IonCard, IonSkeletonText } from "@ionic/react";
+
+// Styles
+import styles from "./Stands.module.css";
+
+// Components
+import ImageSkeleton from "../Image";
 
 const StandsDetailsSkeleton: React.FC = () => {
   return (
-    <div>
-      <h1>Skeleton</h1>
+    <div className={styles.details_container}>
+      <ImageSkeleton className={styles.details_img} />
+
+      <div className={styles.details_name_and_stars}>
+        <IonSkeletonText
+          animated
+          style={{ width: "50%", height: 25, zIndex: 10 }}
+        />
+      </div>
+
+      <IonCard className={styles.details_card}>
+        <IonSkeletonText
+          animated
+          style={{ width: "100%", height: 20, margin: 20 }}
+        />
+
+        <div className={styles.details_description}>
+          <IonSkeletonText animated style={{ width: "100%" }} />
+          <IonSkeletonText animated style={{ width: "100%" }} />
+          <IonSkeletonText animated style={{ width: "100%" }} />
+        </div>
+      </IonCard>
     </div>
   );
 };
