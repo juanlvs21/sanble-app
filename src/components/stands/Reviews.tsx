1795d7581db06d3622a7942b71380706919bad9e
Juan Villarroel <juanlvs97@gmail.com>
2021-03-05 18:50:56 -0400

    Modal to show the details of the stand items

---
+++ src/components/stands/Reviews.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { IonCard } from "@ionic/react";
+
+const StandReviews = () => {
+  return (
+    <IonCard>
+      <h3>Opiniones</h3>
+    </IonCard>
+  );
+};
+
+export default StandReviews;
