a1ec35acb31bd30e1edb9a10f81ad4efc636dc3b
Juan Villarroel <juanlvs97@gmail.com>
2021-10-03 13:48:12 -0400

    Template vuejs with sidebar

--- src/utils/formatCurrency.ts
+++
@@ -1,4 +0,0 @@
-export default (value: number, usd: boolean = false) => {
-  if (usd) return `${value}`.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
-  else return `${value}`.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1.");
-};
