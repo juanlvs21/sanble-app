fa596d5ed80701b869d22fa3dda64dbccbc2bce4
Juan Villarroel <juanlvs97@gmail.com>
2021-11-06 19:04:56 -0400

    The application navbar was created

---
+++ src/components/layouts/MainLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Navbar />
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script>
+import Navbar from "@/components/common/Navbar.vue";
+
+export default {
+  name: "MainLayout",
+  components: { Navbar },
+};
+</script>
+
+<style scoped>
+main {
+  padding: 80px;
+}
+</style>
