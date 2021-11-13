fb6b15670f3702d6506cf109ff87eaf3b8d219c2
Juan Villarroel <juanlvs97@gmail.com>
2021-11-13 18:31:50 -0400

    Create signin and signup views | Configure vuex-persist

--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,18 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+// Plugins
+import { vuexLocal } from "@/plugins/vuex-persist";
+
+// Modules
+import { authModule } from "@/store/modules/auth";
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  // state: {},
+  // mutations: {},
+  // actions: {},
+  modules: {
+    auth: authModule,
+  },
+  plugins: [vuexLocal.plugin],
 });
