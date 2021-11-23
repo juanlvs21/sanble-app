2e3a482cc107a4f874084823c54868c53b8b6a12
Juan Villarroel <juanlvs97@gmail.com>
2021-11-22 21:36:56 -0400

    Changes in the style welcome view | Init layout navbar component

--- src/components/common/Navbar.vue
+++ src/components/common/Navbar.vue
@@ -1,14 +1,13 @@
 <template>
   <nav class="sb__navbar">
-    <vs-navbar color="dark" square text-white>
-      <template #left>
-        <img src="/img/Logo_Horizontal_Light.png" alt="Sanble" height="30px" />
-      </template>
-      <template #right>
-        <vs-button flat>Iniciar Sesión</vs-button>
-        <vs-button>Registrarse</vs-button>
-      </template>
-    </vs-navbar>
+    <div class="sb__navbar-left">
+      <img src="/img/logo_text.svg" width="130px" alt="Sanble" />
+    </div>
+    <div class="sb__navbar-right">
+      <vs-button icon dark circle>
+        <i class="bx bx-bell" />
+      </vs-button>
+    </div>
   </nav>
 </template>
 
@@ -18,16 +17,29 @@ export default {
 };
 </script>
 
-<style>
-.sb__navbar .vs-navbar {
-  height: 50px;
-}
-</style>
-
 <style scoped>
 .sb__navbar {
   position: fixed;
   top: 0;
   width: 100%;
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+}
+.sb__navbar .sb__navbar-left {
+  padding-left: 30px;
+}
+.sb__navbar .sb__navbar-right {
+  padding-right: 30px;
+}
+</style>
+
+<style>
+.sb__navbar .sb__navbar-right .vs-button {
+  width: 41px;
+  height: 41px;
+  background-color: #f4f7f8;
+  color: rgba(var(--vs-text), 1);
 }
 </style>
