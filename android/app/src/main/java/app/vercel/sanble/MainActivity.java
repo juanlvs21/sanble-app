3c27b0ff9c261abed8a7cde2ba4bf2f542600678
Juan Villarroel <juanlvs97@gmail.com>
2021-11-28 18:53:10 -0400

    Add capacitor with react

---
+++ android/app/src/main/java/app/vercel/sanble/MainActivity.java
@@ -0,0 +1,5 @@
+package app.vercel.sanble;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.vercel.sanble',
+  appName: 'sanble',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
