048094d20b638596e7b0841df18eddcb189f9c8d
Juan Villarroel <juanlvs97@gmail.com>
2021-11-29 22:35:51 -0400

    Small changes

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -6,6 +6,7 @@ import { SignupView } from "@/views/auth/Signup";
 
 export const Router = () => (
   <Routes>
+    <Route path="/" element={<Navigate to="/auth/signin" replace />} />
     <Route path="/auth" element={<AuthLayout />}>
       <Route index element={<Navigate to="/auth/signin" replace />} />
       <Route path="signin" element={<SigninView />} />
@@ -14,3 +15,5 @@ export const Router = () => (
     <Route path="*" element={<h1>NOT FOUND</h1>} />
   </Routes>
 );
+
+// https://ui.dev/react-router-code-splitting/
