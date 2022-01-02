6138eb300ee5fb96b8895a925344b053b87ec90c
Juan Villarroel <juanlvs97@gmail.com>
2022-01-01 20:26:08 -0400

    Changes android native

--- android/app/src/main/java/com/sanble/MainActivity.java
+++ android/app/src/main/java/com/sanble/MainActivity.java
@@ -2,7 +2,6 @@ package dev.juanl.sanble;
 
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
-// import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
@@ -17,7 +16,6 @@ public class MainActivity extends ReactActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    // SplashScreen.show(this);
     super.onCreate(savedInstanceState);
   }
 }
--- android/app/src/main/java/com/sanble/MainApplication.java
+++ android/app/src/main/java/com/sanble/MainApplication.java
@@ -10,8 +10,6 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-// import com.oblador.vectoricons.VectorIconsPackage;
-// import org.devio.rn.splashscreen.SplashScreenReactPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -27,9 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new VectorIconsPackage());
-          // packages.add(new SplashScreenReactPackage());
-          
+          // packages.add(new OtherPackage());
           return packages;
         }
 
