5529e614eff930afbfacd00e73404e8a570ca7da
Juan Villarroel <juanlvs97@gmail.com>
2022-09-17 11:14:19 -0400

    Modified styles auth section

--- src/components/layouts/Auth.tsx
+++ src/components/layouts/Auth.tsx
@@ -1,13 +1,27 @@
 import { IonContent, IonGrid, IonRow, IonCol } from "@ionic/react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
 import styles from "./Auth.module.css";
 import TabBar from "@/components/modules/auth/TabBar";
 
+const bgImage: Record<string, string> = {
+  "/app/sesion/entrar": "signinRoute",
+  "/app/sesion/registrar": "signupRoute",
+};
+
+const getWavesClass = (pathname: string) => {
+  const cssClassName = bgImage[pathname];
+  return cssClassName ? styles[cssClassName] : "";
+};
+
 const AuthLayout: React.FC = () => {
+  const { pathname } = useLocation();
+
   return (
-    <IonContent className={styles.layoutContainer}>
-      <IonGrid className={styles.layoutGrid}>
+    <IonContent
+      className={`${styles.layoutContainer} ${getWavesClass(pathname)}`}
+    >
+      <IonGrid className={`${styles.layoutGrid} ${getWavesClass(pathname)}`}>
         <IonRow>
           <IonCol className={styles.logoContainer}>
             <img
--- src/components/modules/auth/TabBar.tsx
+++ src/components/modules/auth/TabBar.tsx
@@ -1,17 +1,39 @@
-import { useMatch, useNavigate } from "react-router-dom";
+import { useMatch, Link } from "react-router-dom";
 
 import styles from "./TabBar.module.css";
 
 const TabBar: React.FC = () => {
+  const matchSignin = useMatch("/app/sesion/entrar");
   const matchSignup = useMatch("/app/sesion/registrar");
-  const navigate = useNavigate();
 
   return (
     <nav className={styles.tabBarContiner}>
+      <ul className={styles.navUl}>
+        <li className={styles.navLi}>
+          <Link
+            to="/app/sesion/registrar"
+            className={`${styles.navLink} ${
+              matchSignup ? styles.linkActive : ""
+            }`}
+          >
+            Registrarse
+          </Link>
+        </li>
+        <li className={styles.navLi}>
+          <Link
+            to="/app/sesion/entrar"
+            className={`${styles.navLink} ${
+              matchSignin ? styles.linkActive : ""
+            }`}
+          >
+            Ingresar
+          </Link>
+        </li>
+      </ul>
       <div className={styles.barContainer}>
         <div
           className={`${styles.barFill} ${
-            matchSignup ? styles.barFillRight : ""
+            matchSignin ? styles.barFillRight : ""
           }`}
         />
       </div>
