a12f4bf2aacbffa1fd1869c8eb548a85f3ba5b87
Juan Villarroel <juanlvs97@gmail.com>
2023-02-22 19:06:09 -0400

    deleted:    src/reportWebVitals.ts

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "@/App";
 import * as serviceWorkerRegistration from "@/serviceWorkerRegistration";
-import reportWebVitals from "@/reportWebVitals";
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
@@ -16,7 +15,3 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 serviceWorkerRegistration.unregister();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-reportWebVitals();
